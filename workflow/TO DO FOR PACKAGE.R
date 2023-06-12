2d960b9e17173d9b9b10c519d268aa47fe32547e
alexpate30 <alexander.pate@manchester.ac.uk>
2023-06-12 11:18:37 +0100

    update vignettes

--- workflow/TO DO FOR PACKAGE.R
+++ workflow/TO DO FOR PACKAGE.R
@@ -3,7 +3,7 @@
 ### DONE 1) Add restricted cubic splines for when using pseudo-values (allows for easier calculation of confidence intervals)
 ### DONE 2) Add tests for pseudo-values
 ### 3) Add a wrapper to help with confidence intervals for calc_calib_blr and calc_calib_mlr. Think I'm actually not going to do this, and allow people to specify their own weights function.
-### 4) Anything to do around sub-models? i.e. re-write the supp_gcc program
+### DONE 4) Anything to do around sub-models? i.e. re-write the supp_gcc program
 ### DONE 5) Rewrite supplementary material (in particular think pseudo values ones should be removed?)
 ### DONE 6) Add more tests for pseudo-values when rcs type curve is included
 ### 7) Add more warnings. A) Is data.mstate and data.raw got the same id's? B) Remove warning about weights and landmark.all = "all", otherwise it comes up when we specify land.mark.type = "all"

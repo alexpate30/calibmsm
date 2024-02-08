bbce67c8f6d0050a3aac35666549ce8659719746
alexpate30 <alexander.pate@manchester.ac.uk>
2024-02-08 16:25:17 +0000

    more control over loess smoother

--- R/internal_calib_pv.R
+++ R/internal_calib_pv.R
@@ -814,16 +814,18 @@ calc_obs_pv_loess_model <- function(pred, pv, data.to.plot,
                                     CI.type){
 
   ### Fit model
-  if (CI != FALSE & CI.type == "parametric"){
-    loess.model <- stats::loess(pv ~ pred,
-                                span = loess.span,
-                                degree = loess.degree,
-                                control = stats::loess.control(surface = loess.surface,
-                                                               statistics = loess.statistics,
-                                                               trace.hat = loess.trace.hat,
-                                                               cell = loess.cell,
-                                                               iterations = loess.iterations,
-                                                               iterTrace = loess.iterTrace))
+  if (CI != FALSE){
+    if (CI.type == parametric){
+      loess.model <- stats::loess(pv ~ pred,
+                                  span = loess.span,
+                                  degree = loess.degree,
+                                  control = stats::loess.control(surface = loess.surface,
+                                                                 statistics = loess.statistics,
+                                                                 trace.hat = loess.trace.hat,
+                                                                 cell = loess.cell,
+                                                                 iterations = loess.iterations,
+                                                                 iterTrace = loess.iterTrace))
+    }
   } else {
     ### If not requiring standard errors for parametric confidence, let statistics = "none" for computational efficiency
     loess.model <- stats::loess(pv ~ pred,
@@ -835,21 +837,22 @@ calc_obs_pv_loess_model <- function(pred, pv, data.to.plot,
                                                                cell = loess.cell,
                                                                iterations = loess.iterations,
                                                                iterTrace = loess.iterTrace))
-    }
+  }
 
 
   ## Calculate predicted observed probabilities (and confidence intervals if requested using parametric approach)
   ## Note we do not calculate standard errors if confidence interval has been requested using the bootstrap (or if no CI requested)
-  ### Fit model
-  if (CI != FALSE & CI.type == "parametric"){
-    ## Predict observed
-    obs <- predict(loess.model, newdata = data.to.plot, se = TRUE)
-    ## Define alpha for CIs
-    alpha <- (1-CI/100)/2
-    ## Put into dataframe
-    obs.data <- data.frame("obs" = obs$fit,
-                           "obs.lower" = obs$fit - stats::qnorm(1-alpha)*obs$se,
-                           "obs.upper" = obs$fit + stats::qnorm(1-alpha)*obs$se)
+  if (CI != FALSE){
+    if (CI.type == parametric){
+      ## Predict observed
+      obs <- predict(loess.model, newdata = data.to.plot, se = TRUE)
+      ## Define alpha for CIs
+      alpha <- (1-CI/100)/2
+      ## Put into dataframe
+      obs.data <- data.frame("obs" = obs$fit,
+                             "obs.lower" = obs$fit - stats::qnorm(1-alpha)*obs$se,
+                             "obs.upper" = obs$fit + stats::qnorm(1-alpha)*obs$se)
+    }
   } else {
     ## Predict observed
     obs <- predict(loess.model, newdata = data.to.plot)

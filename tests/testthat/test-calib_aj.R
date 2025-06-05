5ddfbf4b47693998a6144b0a3392e339517bdce4
alexpate30 <alexander.pate@manchester.ac.uk>
2025-06-05 16:22:54 +0100

    Update test-calib_aj.R

--- tests/testthat/test-calib_aj.R
+++ tests/testthat/test-calib_aj.R
@@ -17,29 +17,29 @@ test_that("check calib_aj, pv_n_pctls = NULL and pv_group_vars = NULL", {
   msebmtcal <- msebmtcal |> dplyr::filter(id %in% 1:50)
 
   ## Calculate observed event probabilities using transitions_out = NULL
-  dat_calib_aj_1 <- calib_msm(data_ms = msebmtcal,
-                              data_raw = ebmtcal,
-                              j = 1,
-                              s = 0,
-                              t = 1826,
-                              tp_pred = tp_pred,
-                              calib_type = 'aj',
-                              tp_pred_plot = NULL, transitions_out = NULL)
+  dat_calib_aj_1 <- suppressWarnings(calib_msm(data_ms = msebmtcal,
+                                               data_raw = ebmtcal,
+                                               j = 1,
+                                               s = 0,
+                                               t = 1826,
+                                               tp_pred = tp_pred,
+                                               calib_type = 'aj',
+                                               tp_pred_plot = NULL, transitions_out = NULL))
 
   expect_equal(class(dat_calib_aj_1), c("calib_aj", "calib_msm"))
   expect_equal(length(dat_calib_aj_1[["mean"]]), 6)
 
   ## Calculate observed event probabilities using transitions_out = NULL
-  dat_calib_aj_CI_1 <- calib_msm(data_ms = msebmtcal,
-                              data_raw = ebmtcal,
-                              j = 1,
-                              s = 0,
-                              t = 1826,
-                              tp_pred = tp_pred,
-                              calib_type = 'aj',
-                              CI = 95,
-                              CI_R_boot = 10,
-                              tp_pred_plot = NULL, transitions_out = NULL)
+  dat_calib_aj_CI_1 <- suppressWarnings(calib_msm(data_ms = msebmtcal,
+                                                  data_raw = ebmtcal,
+                                                  j = 1,
+                                                  s = 0,
+                                                  t = 1826,
+                                                  tp_pred = tp_pred,
+                                                  calib_type = 'aj',
+                                                  CI = 95,
+                                                  CI_R_boot = 10,
+                                                  tp_pred_plot = NULL, transitions_out = NULL))
 
   expect_equal(class(dat_calib_aj_CI_1), c("calib_aj", "calib_msm"))
   expect_equal(length(dat_calib_aj_CI_1[["mean"]]), 6)
@@ -65,31 +65,31 @@ test_that("check calib_pv output, pv_n_pctls specified", {
   msebmtcal <- msebmtcal |> dplyr::filter(id %in% 1:50)
 
   ## Calculate observed event probabilities using transitions_out = NULL
-  dat_calib_aj_1 <- calib_msm(data_ms = msebmtcal,
-                              data_raw = ebmtcal,
-                              j = 1,
-                              s = 0,
-                              t = 1826,
-                              tp_pred = tp_pred,
-                              calib_type = 'aj',
-                              pv_n_pctls = 2,
-                              tp_pred_plot = NULL, transitions_out = NULL)
+  dat_calib_aj_1 <- suppressWarnings(calib_msm(data_ms = msebmtcal,
+                                               data_raw = ebmtcal,
+                                               j = 1,
+                                               s = 0,
+                                               t = 1826,
+                                               tp_pred = tp_pred,
+                                               calib_type = 'aj',
+                                               pv_n_pctls = 2,
+                                               tp_pred_plot = NULL, transitions_out = NULL))
 
   expect_equal(class(dat_calib_aj_1), c("calib_aj", "calib_msm"))
   expect_equal(length(dat_calib_aj_1[["mean"]]), 6)
 
   ## Calculate observed event probabilities using transitions_out = NULL
-  dat_calib_aj_CI_1 <- calib_msm(data_ms = msebmtcal,
-                                 data_raw = ebmtcal,
-                                 j = 1,
-                                 s = 0,
-                                 t = 1826,
-                                 tp_pred = tp_pred,
-                                 calib_type = 'aj',
-                                 pv_n_pctls = 2,
-                                 CI = 95,
-                                 CI_R_boot = 10,
-                                 tp_pred_plot = NULL, transitions_out = NULL)
+  dat_calib_aj_CI_1 <- suppressWarnings(calib_msm(data_ms = msebmtcal,
+                                                  data_raw = ebmtcal,
+                                                  j = 1,
+                                                  s = 0,
+                                                  t = 1826,
+                                                  tp_pred = tp_pred,
+                                                  calib_type = 'aj',
+                                                  pv_n_pctls = 2,
+                                                  CI = 95,
+                                                  CI_R_boot = 10,
+                                                  tp_pred_plot = NULL, transitions_out = NULL))
 
   expect_equal(class(dat_calib_aj_CI_1), c("calib_aj", "calib_msm"))
   expect_equal(length(dat_calib_aj_CI_1[["mean"]]), 6)
@@ -117,31 +117,31 @@ test_that("check calib_pv output, pv_group_vars specified", {
   msebmtcal <- msebmtcal |> dplyr::filter(id %in% 1:50)
 
   ## Calculate observed event probabilities using transitions_out = NULL
-  dat_calib_aj_1 <- calib_msm(data_ms = msebmtcal,
-                              data_raw = ebmtcal,
-                              j = 1,
-                              s = 0,
-                              t = 1826,
-                              tp_pred = tp_pred,
-                              calib_type = 'aj',
-                              pv_group_vars = c("year"),
-                              tp_pred_plot = NULL, transitions_out = NULL)
+  dat_calib_aj_1 <- suppressWarnings(calib_msm(data_ms = msebmtcal,
+                                               data_raw = ebmtcal,
+                                               j = 1,
+                                               s = 0,
+                                               t = 1826,
+                                               tp_pred = tp_pred,
+                                               calib_type = 'aj',
+                                               pv_group_vars = c("year"),
+                                               tp_pred_plot = NULL, transitions_out = NULL))
 
   expect_equal(class(dat_calib_aj_1), c("calib_aj", "calib_msm"))
   expect_equal(length(dat_calib_aj_1[["mean"]]), 6)
 
   ## Calculate observed event probabilities using transitions_out = NULL
-  dat_calib_aj_CI_1 <- calib_msm(data_ms = msebmtcal,
-                                 data_raw = ebmtcal,
-                                 j = 1,
-                                 s = 0,
-                                 t = 1826,
-                                 tp_pred = tp_pred,
-                                 calib_type = 'aj',
-                                 pv_group_vars = c("year"),
-                                 CI = 95,
-                                 CI_R_boot = 10,
-                                 tp_pred_plot = NULL, transitions_out = NULL)
+  dat_calib_aj_CI_1 <- suppressWarnings(calib_msm(data_ms = msebmtcal,
+                                                  data_raw = ebmtcal,
+                                                  j = 1,
+                                                  s = 0,
+                                                  t = 1826,
+                                                  tp_pred = tp_pred,
+                                                  calib_type = 'aj',
+                                                  pv_group_vars = c("year"),
+                                                  CI = 95,
+                                                  CI_R_boot = 10,
+                                                  tp_pred_plot = NULL, transitions_out = NULL))
 
   expect_equal(class(dat_calib_aj_CI_1), c("calib_aj", "calib_msm"))
   expect_equal(length(dat_calib_aj_CI_1[["mean"]]), 6)
@@ -172,33 +172,33 @@ test_that("check calib_pv output, pv_group_vars and pv_n_pctls specified ", {
   msebmtcal <- msebmtcal |> dplyr::filter(id %in% 1:100)
 
   ## Calculate observed event probabilities using transitions_out = NULL
-  dat_calib_aj_1 <- calib_msm(data_ms = msebmtcal,
-                              data_raw = ebmtcal,
-                              j = 1,
-                              s = 0,
-                              t = 1826,
-                              tp_pred = tp_pred,
-                              calib_type = 'aj',
-                              pv_n_pctls = 2,
-                              pv_group_vars = c("year"),
-                              tp_pred_plot = NULL, transitions_out = NULL)
+  dat_calib_aj_1 <- suppressWarnings(calib_msm(data_ms = msebmtcal,
+                                               data_raw = ebmtcal,
+                                               j = 1,
+                                               s = 0,
+                                               t = 1826,
+                                               tp_pred = tp_pred,
+                                               calib_type = 'aj',
+                                               pv_n_pctls = 2,
+                                               pv_group_vars = c("year"),
+                                               tp_pred_plot = NULL, transitions_out = NULL))
 
   expect_equal(class(dat_calib_aj_1), c("calib_aj", "calib_msm"))
   expect_equal(length(dat_calib_aj_1[["mean"]]), 6)
 
   ## Calculate observed event probabilities using transitions_out = NULL
-  dat_calib_aj_CI_1 <- calib_msm(data_ms = msebmtcal,
-                                 data_raw = ebmtcal,
-                                 j = 1,
-                                 s = 0,
-                                 t = 1826,
-                                 tp_pred = tp_pred,
-                                 calib_type = 'aj',
-                                 pv_n_pctls = 2,
-                                 pv_group_vars = c("year"),
-                                 CI = 95,
-                                 CI_R_boot = 10,
-                                 tp_pred_plot = NULL, transitions_out = NULL)
+  dat_calib_aj_CI_1 <- suppressWarnings(calib_msm(data_ms = msebmtcal,
+                                                  data_raw = ebmtcal,
+                                                  j = 1,
+                                                  s = 0,
+                                                  t = 1826,
+                                                  tp_pred = tp_pred,
+                                                  calib_type = 'aj',
+                                                  pv_n_pctls = 2,
+                                                  pv_group_vars = c("year"),
+                                                  CI = 95,
+                                                  CI_R_boot = 10,
+                                                  tp_pred_plot = NULL, transitions_out = NULL))
 
   expect_equal(class(dat_calib_aj_CI_1), c("calib_aj", "calib_msm"))
   expect_equal(length(dat_calib_aj_CI_1[["mean"]]), 6)
